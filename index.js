26f8050753ebbfb62c2d9de579c7e3c14d89d9b0
FabianoRizzi <fabianorizzi@gmail.com>
2026-06-04 09:01:41 -0300

    git init

---
+++ index.js
@@ -0,0 +1,28 @@
+//criando a função
+function calcularVitorias(vitorias, derrotas) {
+  let saldoVitorias = vitorias - derrotas;
+  let nivel = "";
+
+  if (vitorias < 10) {
+    nivel = "Ferro";
+  } else if (vitorias <= 20) {
+    nivel = "Bronze";
+  } else if (vitorias <= 50) {
+    nivel = "Prata";
+  } else if (vitorias <= 80) {
+    nivel = "Ouro";
+  } else if (vitorias <= 90) {
+    nivel = "Diamante";
+  } else if (vitorias <= 100) {
+    nivel = "Lendário";
+  } else if (vitorias >= 101) {
+    nivel = "Imortal";
+  }
+
+  console.log(
+    `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`
+  );
+}
+
+// Exemplo de uso
+calcularVitorias(75, 20);
